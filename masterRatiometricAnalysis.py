d80b0d74c37190c1e04d5c9ee0cb7d654f7ba34b
monikamakurath <monikamakurath@gmail.com>
2024-11-24 18:32:25 -0500

    making sure this version is saved,  renamed steps in the master file

--- masterRatiometricAnalysis.py
+++ masterRatiometricAnalysis.py
@@ -6,26 +6,26 @@ import fifthStepRatioAnalysisPlots
 import sixthStepRatioAnalysisImageStack
 
 def main():
-    print("Starting first step of ratiometric image analysis: Selecting the data file and exporting pre-registration.tiff stack")
+    print("First: Selecting the data file and exporting pre-registration.tiff stack")
     file_path, output_dir = firstStepLoadFile.firstStepLoadFile()
 
     if file_path and output_dir:
-        print("Starting second step: Registering image stack using the loaded file...")
+        print("Second: Registering image stack using the loaded file...")
         secondStepRegistration.secondStepRegistration(file_path, output_dir)
 
-        print("Starting third step: Summing intensities of registered image stack...")
+        print("Third: Summing intensities of registered image stack...")
         thirdStepSumChannels.thirdStepSumChannels(output_dir)
 
-        print("Starting fourth step: Creating segmentation masks...")
+        print("Fourth: Creating segmentation masks...")
         fourthStepSegmentation.fourthStepSegmentation(output_dir)
 
-        print("Starting fifth step: Performing ratiometric analysis...")
-        frame_rate = 9.65  # in seconds. Set the frame rate manually here or prompt the user if needed
+        print("Fifth: Performing ratiometric analysis...")
+        frame_rate = 59.86  # in seconds. Set the frame rate manually here or prompt the user if needed
         fifthStepRatioAnalysisPlots.fifthStepRatioAnalysisPlots(output_dir, frame_rate)
 
-        print("Starting sixth step: Generating ratiometric images with annotations...")
-        pixel_width = 0.0425232
-        scale_bar_length_microns = 1
+        print("Sisth: Generating ratiometric images with annotations...")
+        pixel_width = 0.0425270
+        scale_bar_length_microns = 10
         high_dpi = 300
         sixthStepRatioAnalysisImageStack.sixthStepRatioAnalysisImageStack(output_dir, frame_rate, pixel_width, scale_bar_length_microns, high_dpi)
 
